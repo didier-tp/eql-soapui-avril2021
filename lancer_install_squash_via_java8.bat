9e0ac31866305c27c2dab8005f784ebe49267621
didier <didier@d-defrance.fr>
2021-04-09 11:41:26 +0200

    bat

---
+++ lancer_install_squash_via_java8.bat
@@ -0,0 +1,4 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_211
+set PATH=%JAVA_HOME%\bin;%PATH%
+REM attention l'installeur ne fonctionne pas bien avec java11
+java -jar squash-tm-1.22.2.RELEASE-installer.jar
